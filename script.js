f8da8988b3f4e3bfb2294b7f56b8dc3ba9aebfc7
Bas <bmjdekker@gmail.com>
2021-03-03 11:42:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const age = 24;
+const isFemale = true;
+const driverStatus = "bob";
+const Name = "Bas";
+const totalAmount = 101;
+
+if (age >= 18) {
+  console.log("Loop maar door naar binnen");
+} else {
+  console.log("Je bent te jong, je mag niet naar binnen");
+}
+
+if (isFemale) {
+  console.log("Je bent een vrouw, loop maar door naar binnen");
+} else {
+  console.log("Het is ladies night, je mag helaas niet naar binnen");
+}
+
+if (driverStatus === "bob") {
+  console.log("Hier heb je een cassis. Je mag rijden.");
+} else {
+  console.log("Hier heb je een biertje. Je mag niet rijden.");
+}
+
+if (age >= 18 && age <= 25) {
+  console.log("Je krijgt 50% korting!");
+} else {
+  console.log("Jammer, je krijgt geen 50% korting");
+}
+
+if (Name === "Bram" || Name === "Sarah") {
+  console.log("Je krijgt een gratis biertje!");
+} else {
+  console.log("Jammer, je krijgt geen gratis biertje");
+}
+
+if (totalAmount >= 25 && <= 50) {
+  console.log("Je krijgt een gratis portie vega bitterballen!");
+} else if (totalAmount >= 50 && <= 100) {
+  console.log("Je krijgt een gratis portie nachos!");
+} else if (totalAmount >= 100) {
+  console.log("Je krijgt een gratis flesje champagne!");
+}
